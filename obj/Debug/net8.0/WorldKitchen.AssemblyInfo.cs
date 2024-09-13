51ad15710207ed35ace90de7666699e847bc720c
David <137923756+David-Melik@users.noreply.github.com>
2024-09-13 08:42:51 +0200

    Search Bar is clever now the backend finish

--- obj/Debug/net8.0/WorldKitchen.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorldKitchen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldKitchen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e63f590d1fffe7a93afb8ab9e130f181997f8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd1890f9c11604bc9209b6928d4235e82bdea803")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldKitchen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldKitchen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,23 @@
 ﻿var isClicked = localStorage.getItem("isClicked") === "true";
+var isUrlCaptured = localStorage.getItem("isUrlCaptured") === "true";
+var currentUrl; // Declare currentUrl in global scope
 
 function openSearch() {
   document.getElementById("searchInput").style.display = "none";
   document.getElementById("myOverlay").style.display = "block";
   var inputField = document.getElementById("focusSearch");
+
+  // Capture the URL only if it hasn't been captured yet
+  if (!isUrlCaptured) {
+    currentUrl = window.location.href; // Capture the current page URL
+    localStorage.setItem("currentUrl",
+                         currentUrl); // Store the URL in localStorage
+    localStorage.setItem("isUrlCaptured", "true"); // Mark URL as captured
+  } else {
+    // Retrieve the URL from localStorage if it has already been captured
+    currentUrl = localStorage.getItem("currentUrl");
+  }
+
   inputField.disabled = false;
   inputField.focus();
   isClicked = true; // Set the variable to true
@@ -12,11 +26,16 @@ function openSearch() {
 }
 
 function closeSearch() {
+  window.location.href = currentUrl; // Redirect to the stored URL
   document.getElementById("searchInput").style.display = "block";
   document.getElementById("myOverlay").style.display = "none";
   isClicked = false; // Reset the variable to false
   localStorage.setItem("isClicked",
                        isClicked); // Store the updated value in localStorage
+
+  // Reset URL and flag
+  localStorage.removeItem("currentUrl");
+  localStorage.removeItem("isUrlCaptured");
 }
 
 // Function to check if isClicked is true on page load and open the overlay if
