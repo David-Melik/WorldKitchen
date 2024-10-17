cd76f79b67b2905409ab0c5e05271d73dcf72537
David <137923756+David-Melik@users.noreply.github.com>
2024-10-17 15:13:33 +0200

    some change frontend index

--- obj/Debug/net8.0/WorldKitchen.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorldKitchen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldKitchen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+757c7ab92e793e599c3a98ccc178906a27c7dd3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d6aacbddcb937e13a1ea4d637fe94641dd922b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldKitchen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldKitchen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
