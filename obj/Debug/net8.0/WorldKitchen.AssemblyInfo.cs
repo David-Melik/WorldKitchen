926c8a99606d23153dab0e0525105463adffa0af
David <137923756+David-Melik@users.noreply.github.com>
2024-10-19 12:33:34 +0200

    bfore update

--- obj/Debug/net8.0/WorldKitchen.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorldKitchen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldKitchen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf1a87d2aec665339ba086b323853c33fcc4225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ca286aaf30fdd1891b0d08cb80b6d9a81c89ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldKitchen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldKitchen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
